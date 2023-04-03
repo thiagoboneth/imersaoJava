0e6aceaa723db1c6c3eb90cab5953eda20bce302
thiagoboneth <thiago.luis@mercadolivre.com>
2023-04-03 01:40:01 -0300

    Gerador de sticker implementado com poster e nome do filme

--- src/App.java
+++ src/App.java
@@ -1,4 +1,6 @@
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -6,13 +8,15 @@ import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
 import java.util.Map;
 
+import service.JsonParse;
+
 public class App {
     public static void main(String[] args) throws Exception {
       
       /*
        * Melhorar estrurura e ocultar chave de acesso
        */
-      String url = ("https://imdb-api.com/en/API/Top250Movies/{chaveDeAcesso}");
+      String url = ("https://imdb-api.com/en/API/Top250Movies/{chave}");
       URI endereco = URI.create(url);
       var client = HttpClient.newHttpClient();
       HttpRequest request = HttpRequest.newBuilder(endereco).GET().build();
@@ -24,12 +28,18 @@ public class App {
 
       System.out.println(listaDeFilmes.size());
       System.out.println(listaDeFilmes.get(0));
+  
 
       for (Map<String,String> filme : listaDeFilmes) {
-        System.out.println(filme.get("fullTitle"));
-        System.out.println(filme.get("image"));
-        System.out.println(filme.get("imDbRating"));
-        System.out.println();
+        String urlImagem = filme.get("image");
+        String titulo = filme.get("title");
+
+        InputStream inputStream = new URL(urlImagem).openStream();
+        String nomeDoArquivo = titulo +".png";
+
+        var gerador = new GeradorDeFigurinhas();
+        gerador.cria(inputStream, nomeDoArquivo, titulo);
+      
       }
     }
 }
